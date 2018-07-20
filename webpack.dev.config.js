5734e79bcf6f2c9c78ab64a210b74e39b3d59a87
Alex Strand <ajstrand8@gmail.com>
2018-07-20 18:58:59 -0400

    basic functionality working, first commit. need to add delete button

---
+++ webpack.dev.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode:"development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[c|a]ss$/,
+        exclude: "/node_modules/",
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+};
